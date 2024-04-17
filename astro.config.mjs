4791e0eda174a837eec09d1eb9a794d4edd7b2da
Sanatan <sanatan.iit@gmail.com>
2024-04-17 11:59:21 +0530

    build err

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
+
 
 // https://astro.build/config
 export default defineConfig({
